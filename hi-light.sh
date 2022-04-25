0f1f55984975a2655a011a2a50e33bb4d492cc18
The ELCI Group <88078492+ELCI-Linux@users.noreply.github.com>
2022-04-25 21:58:55 +0100

    Add files via upload

--- hi-light.sh
+++ hi-light.sh
@@ -1,10 +1,14 @@
 #! /bin/bash/
-
-
 #Hi-light has been designed to manually modify screen brightness using brightnessctl using super user permissions
 
-	brightness=$(zenity --scale --min-value=0 --max-value=100 --step=5 \
-	--title="Hi-light screen brightness adjustment" \
+	helper="Hi-Light screen brightness adjustment"
+	versnum="2.0"
+
+
+	current=$(brightnessctl | grep -oP '(?<=\()[^\%]+')
+
+	brightness=$(zenity --scale --min-value=0 --value=$current --max-value=100 --step=5 \
+	--title="Hi-light screen brightness adjustment $versnum" \
 	--text="Please select your screen brightness level, adjustment will be visibile after confirmation")
 
 	sudo brightnessctl s $brightness%
